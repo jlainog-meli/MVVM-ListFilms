1244b5ee6e5aa2cdfd4c74f3eb9027c5b3ec5c9d
Lucas Carvalho <empresarial.lucascarvalho@gmail.com>
2022-03-27 23:07:05 -0300

    Base Project

---
+++ MVVM-ListFilms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-ListFilms
+//
+//  Created by Lucas De Castro Carvalho on 27/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
